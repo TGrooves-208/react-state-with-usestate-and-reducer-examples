6313b01f66ef7fe20bdbac5c996f9ce66ad11982
GAguilar208 <gaguilar208.accessibility@gmail.com>
2022-12-27 19:42:48 -0700

    feat: useState and useReducer examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useState } from "react";
+import "./App.css";
 
-function App() {
+// The state is the old state, and the action is what is being changed
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { cafe: state.cafe + action.num };
+    case "decrement":
+      return { cafe: state.cafe - action.num };
+    default:
+      throw new Error("Unkown action type this is wack");
+  }
+}
+
+export default function App() {
+  // I personally like useState over using useReducer
+  // This gives an example on how to use both
+  // Wouldn't advise on using both in the same manner as this is an example
+  const [coffee, setCoffee] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    cafe: 0,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Coffee coffee={coffee} setCoffee={setCoffee} />
+      <Cafe
+        cafe={state.cafe}
+        onClick={() => dispatch({
+          type: "increment",
+          num: 1,
+        })}
+      />
+      {/* Uncomment this below to see the error as we aren't lifting state up at all */}
+      {/* <Coffee /> */}
+    </>
   );
 }
 
-export default App;
+function Coffee({ coffee, setCoffee }) {
+  // const [coffee, setCoffee] = useState(0);
+  return (
+    <>
+      <button onClick={() => setCoffee(coffee + 1)}>More Coffee!!!</button>
+      <p>Coffee Count: {coffee} </p>
+    </>
+  );
+}
+
+function Cafe({ cafe, onClick }) {
+  return (
+    <>
+      <button onClick={onClick}>Add more cafecito</button>
+      <p>Cafecito Count: {cafe}</p>
+    </>
+  );
+}
